d45a4afbe98ef62773d1272e06286697ced56bcb
RobertSGomes <robertsg.dev@gmail.com>
2026-01-31 17:54:12 -0300

    feat: crud de veículos completo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-import { COMPONENTS_IMPORTS } from './shared/components';
-
 @Component({
   selector: 'app-root',
-  imports: [COMPONENTS_IMPORTS, RouterOutlet],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
